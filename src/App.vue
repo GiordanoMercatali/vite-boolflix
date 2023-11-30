5c81e2ac16b8d8dd62e85e8916327a0c1bfb3ad5
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-30 10:10:12 +0100

    add: getmovies method

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,31 @@
     
   </div>
 </template>
+
 <script>
+import axios from 'axios';
+import {store} from './store.js';
+// console.log(store);
 export default {
-  
+  components: {AppHeader},
+  data(){
+    return{
+      store,
+    };
+  },
+  methods: {
+    getMovies(){
+      axios.get(this.store.moviesUrl + '?api_key=' + this.store.apiKey + '&query=' + this.store.searchText)
+      .then(resp => {
+        this.store.moviesArray = resp.data.results
+      })
+    }
+  }
 }
 </script>
+
 <style lang="scss" scoped>
+/* @use "./style/general.scss"
+@import "@fontawesome/fontawesome-free/css/all.css" */
   
 </style>
